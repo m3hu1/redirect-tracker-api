eefdf6e9bc8a9e43dba7ee6055561174ac0df727
m3hu1 <pathakkmehul@gmail.com>
2024-04-10 17:19:35 +0530

    temp commit

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 #/api/api.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
 import requests
+import os
+
+api_key = os.getenv("API_KEY")
 
 app = FastAPI()
 
@@ -31,3 +34,18 @@ def check_redirects(url: str):
         "status_code": final_status_code,
         "redirects": redirects
     }
+
+@app.get("/check-ip-reputation")
+def check_ip_reputation(ip: str, x_key: str = Header(None)):
+    try:
+        if x_key != api_key:
+            return {"error": "Invalid API key"}
+        
+        # Replace 'YOUR_API_KEY' with your actual AbuseIPDB API key
+        response = requests.get(f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}", 
+                                headers={"Key": "44537bf504d736ece21c79b60c23a8bb2254d1508e255501d29139194b0fd4774074afed10c5192e", 
+                                         "Accept": "application/json"})
+        data = response.json()
+        return data
+    except Exception as e:
+        return {"error": str(e)}
