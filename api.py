525edc6a78775c9520442693254366787465ec48
m3hu1 <pathakkmehul@gmail.com>
2024-04-11 13:38:33 +0530

    added check-ssl func

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 #/api/api.py
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-from io import BytesIO
 
 app = FastAPI()
 
@@ -36,9 +35,6 @@ def check_redirects(url: str):
 @app.get("/check-ip-reputation")
 def check_ip_reputation(ip: str, x_key: str = Header(None)):
     try:
-        # if x_key != "44537bf504d736ece21c79b60c23a8bb2254d1508e255501d29139194b0fd4774074afed10c5192e":
-        #     return {"error": "Invalid API key"}
-        
         response = requests.get(f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}", 
                                 headers={"Key": "44537bf504d736ece21c79b60c23a8bb2254d1508e255501d29139194b0fd4774074afed10c5192e", 
                                          "Accept": "application/json"})
@@ -47,25 +43,38 @@ def check_ip_reputation(ip: str, x_key: str = Header(None)):
     except Exception as e:
         return {"error": str(e)}
 
-@app.post("/check-url")
-async def check_url(url: str, x_key: str = Header(None)):
+@app.get("/check-ssl")
+def check_ssl(url: str):
     try:
-        if x_key != "YOUR_API_KEY":
-            raise HTTPException(status_code=401, detail="Invalid API key")
-
-        response = requests.get(url)
-        if response.status_code != 200:
-            raise HTTPException(status_code=400, detail="Failed to download file from URL")
-
-        file_data = BytesIO(response.content)
-        files = {"file": ("filename", file_data)}
-
         headers = {
-            "x-apikey": "04b608018caed843361d911f92413804b38f07f83e68d404661047fd90840c04",
+            "X-RapidAPI-Key": "876ec4f793msh46e01bc9724cf74p127786jsn2d01e3fe7ae0",
+            "X-RapidAPI-Host": "ssl-certificate-checker2.p.rapidapi.com"
         }
 
-        vt_response = requests.post("https://www.virustotal.com/api/v3/files", headers=headers, files=files)
-        return vt_response.json()
-    
+        querystring = {"host": url}
+
+        response = requests.get("https://ssl-certificate-checker2.p.rapidapi.com/ssl-certificate-checker/check",
+                                headers=headers, params=querystring)
+
+        data = response.json()
+
+        cert_info = {
+            "subject": data["subject"]["CN"],
+            "issuer": data["issuer"]["CN"],
+            "valid_from": data["validFrom"],
+            "valid_until": data["validTo"],
+            "expires_in_days": data["expiresInDays"],
+            "fingerprint": data["fingerprint"],
+            "fingerprint256": data["fingerprint256"],
+            "serial_number": data["serialNumber"],
+            "pem": data["pem"],
+            "protocol": data["protocol"],
+            "cipher_name": data["cipher"]["name"],
+            "cipher_standard_name": data["cipher"]["standardName"],
+            "cipher_version": data["cipher"]["version"],
+            "ocsp_uri": data["infoAccess"]["OCSP - URI"],
+            "ca_issuers_uri": data["infoAccess"]["CA Issuers - URI"]
+        }
+        return cert_info
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"error": str(e)}
